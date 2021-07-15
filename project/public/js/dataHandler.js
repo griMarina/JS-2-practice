7362881dc036cdb18bac985c3538b7f797f147a9
marina <mgrigoreva12@gmail.com>
2021-07-15 10:51:23 +0300

    new project

---
+++ project/public/js/dataHandler.js
@@ -0,0 +1,71 @@
+// модуль, который получает данные с сервера
+
+export default {
+    _url: '/api/v1/',
+
+    getCatalog(errorCallback) {
+        return fetch(`${this._url}catalogData.json`)
+            .then((response) => {
+                if (response.ok) {
+                    return response.data;
+                } else {
+                    return errorCallback();
+                }
+            })
+            .catch((error) => {
+                return errorCallback();
+            })
+    },
+
+    // получить товары, которые пользователь добавил в корзину
+    getCart(errorCallback) {
+        return fetch(`${this._url}getCart.json`)
+            .then((response) => {
+                if (response.ok) {
+                    return response.data;
+                } else {
+                    return errorCallback(response.status);
+                }
+            })
+            .catch((error) => {
+                return errorCallback(error);
+            })
+    },
+
+    // добавляем товар в корзину через сервер
+    addToCart(errorCallback, data) {
+        return fetch(`${this._url}addToCart`, {
+            method: 'POST',
+            body: data
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return true;
+                } else {
+                    errorCallback(response.status);
+                }
+            })
+            .catch((error) => {
+                return errorCallback(error);
+            })
+    },
+
+    // удалить товар из корзины
+    deleteFromCart(errorCallback, id) {
+        return fetch(`${this._url}addToCart`, {
+            method: 'DELETE',
+            body: { id: id }
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return true;
+                } else {
+                    errorCallback(response.status);
+                }
+            })
+            .catch((error) => {
+                return errorCallback(error);
+            })
+    }
+
+}
