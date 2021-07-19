b4b059281f5f52c72f260994d6d4164ea012f295
marina <mgrigoreva12@gmail.com>
2021-07-19 12:26:42 +0300

    totalPrice & totalQuantity

--- project-vue/main.js
+++ project-vue/main.js
@@ -75,7 +75,7 @@ Vue.component('site-header', {
                     <path d="M26.2009 29C25.5532 28.9738 24.9415 28.6948 24.4972 28.2227C24.0529 27.7506 23.8114 27.1232 23.8245 26.475C23.8376 25.8269 24.1043 25.2097 24.5673 24.7559C25.0303 24.3022 25.6527 24.048 26.301 24.048C26.9493 24.048 27.5717 24.3022 28.0347 24.7559C28.4977 25.2097 28.7644 25.8269 28.7775 26.475C28.7906 27.1232 28.549 27.7506 28.1047 28.2227C27.6604 28.6948 27.0488 28.9738 26.401 29H26.2009ZM6.75293 26.32C6.75293 25.79 6.91011 25.2718 7.20459 24.8311C7.49907 24.3904 7.91764 24.0469 8.40735 23.844C8.89705 23.6412 9.43594 23.5881 9.95581 23.6915C10.4757 23.7949 10.9532 24.0502 11.328 24.425C11.7028 24.7998 11.9581 25.2773 12.0615 25.7972C12.1649 26.317 12.1118 26.8559 11.9089 27.3456C11.7061 27.8353 11.3626 28.2539 10.9219 28.5483C10.4812 28.8428 9.96304 29 9.43298 29C9.08087 29.0003 8.73212 28.9311 8.40674 28.7966C8.08136 28.662 7.78569 28.4646 7.53662 28.2158C7.28755 27.9669 7.09001 27.6713 6.9552 27.3461C6.82039 27.0208 6.75098 26.6721 6.75098 26.32H6.75293ZM10.553 20.686C10.2935 20.6868 10.0409 20.6024 9.83411 20.4457C9.62727 20.2891 9.47758 20.0689 9.40796 19.819L4.57495 2.36401H1.18201C0.868521 2.36401 0.567859 2.23947 0.346191 2.01781C0.124523 1.79614 0 1.49549 0 1.18201C0 0.868519 0.124523 0.567873 0.346191 0.346205C0.567859 0.124537 0.868521 5.81268e-06 1.18201 5.81268e-06H5.46301C5.7225 -0.00080736 5.97504 0.0837201 6.18176 0.240568C6.38848 0.397416 6.53784 0.617884 6.60693 0.868006L11.4399 18.323H24.6179L29.001 8.27501H14.401C14.2428 8.27961 14.0854 8.25242 13.9379 8.19507C13.7904 8.13771 13.6559 8.05134 13.5424 7.94108C13.4288 7.83082 13.3386 7.69891 13.277 7.55315C13.2154 7.40739 13.1836 7.25075 13.1836 7.0925C13.1836 6.93426 13.2154 6.77762 13.277 6.63186C13.3386 6.4861 13.4288 6.35419 13.5424 6.24393C13.6559 6.13367 13.7904 6.0473 13.9379 5.98994C14.0854 5.93259 14.2428 5.90541 14.401 5.91001H30.814C31.0097 5.90996 31.2022 5.95866 31.3744 6.05172C31.5465 6.14478 31.6928 6.27926 31.7999 6.44301C31.9078 6.60729 31.9734 6.79569 31.9908 6.99145C32.0083 7.18721 31.9771 7.38424 31.9 7.565L26.495 19.977C26.4026 20.1876 26.251 20.3668 26.0585 20.4927C25.866 20.6186 25.641 20.6858 25.411 20.686H10.553Z" fill="#E8E8E8"/>
                     </svg>
                     <img class="cart-circle" src="img/cart-circle.svg" alt="cart">
-                    <span>{{count}}</span>
+                    <span>{{totalQuantity}}</span>
             </a>
         </div>
     </div>
@@ -89,7 +89,7 @@ Vue.component('site-header', {
 
         }
     },
-    props: ['count']
+    props: ['total-quantity']
 })
 
 Vue.component('promo', {
@@ -266,52 +266,63 @@ Vue.component('cart-card', {
             <li class="purchasing__list-item">Price: <span class="purchasing__text-price">$ {{cartProduct.price}}</span></li>
             <li class="purchasing__list-item">Color: Red</li>
             <li class="purchasing__list-item">Size: Xl</li>
-            <li class="purchasing__list-item">Quantity:	<input class="purchasing__list-input" type="number" min="0" max="20"></li>
+            <li class="purchasing__list-item">Quantity:	<input class="purchasing__list-input" type="number" min="0" max="20" :value=cartProduct.quantity></li>
         </ul>
-        <button class="purchasing__btn-close"><svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <button @click="removeFromCartHandler" class="purchasing__btn-close"><svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M11.2453 9L17.5302 2.71516C17.8285 2.41741 17.9962 2.01336 17.9966 1.59191C17.997 1.17045 17.8299 0.76611 17.5322 0.467833C17.2344 0.169555 16.8304 0.00177586 16.4089 0.00140366C15.9875 0.00103146 15.5831 0.168097 15.2848 0.465848L9 6.75069L2.71516 0.465848C2.41688 0.167571 2.01233 0 1.5905 0C1.16868 0 0.764125 0.167571 0.465848 0.465848C0.167571 0.764125 0 1.16868 0 1.5905C0 2.01233 0.167571 2.41688 0.465848 2.71516L6.75069 9L0.465848 15.2848C0.167571 15.5831 0 15.9877 0 16.4095C0 16.8313 0.167571 17.2359 0.465848 17.5342C0.764125 17.8324 1.16868 18 1.5905 18C2.01233 18 2.41688 17.8324 2.71516 17.5342L9 11.2493L15.2848 17.5342C15.5831 17.8324 15.9877 18 16.4095 18C16.8313 18 17.2359 17.8324 17.5342 17.5342C17.8324 17.2359 18 16.8313 18 16.4095C18 15.9877 17.8324 15.5831 17.5342 15.2848L11.2453 9Z" fill="#575757"/>
             </svg>
         </button>
     </div>
 </div>`,
-    props: ['cartProduct']
+    props: ['cartProduct'],
+    methods: {
+        removeFromCartHandler() {
+            this.$emit('remove-from-cart', this.cartProduct.id)
+        }
+    }
 
 })
 
-Vue.component('purchasing', {
-    template: `<section class="purchasing center">
-    <div class="purchasing__box">
+Vue.component('cart-products-list', {
+    template: `<div class="purchasing__box">
 
-        <cart-card v-for="cartProduct of cart" v-bind:cartProduct="cartProduct"></cart-card>
+        <cart-card v-for="cartProduct of cart" v-bind:cartProduct="cartProduct" @remove-from-cart="removeFromCartHandler"></cart-card>
 
         <div class="purchasing__btn">
             <button>Clear shopping cart</button>
             <a href="catalog.html">Continue shopping</a>
         </div>
-    </div>
-    <div class="purchasing__confirm">
-                <form class="purchasing__form" action="#" method="POST">
-                    <span class="purchasing__form-heading">SHIPPING ADRESS</span>
-                    <input type="text" class="purchasing__form-inp" placeholder="Bangladesh">
-                    <input type="text" class="purchasing__form-inp" placeholder="State">
-                    <input type="text" class="purchasing__form-inp" placeholder="Postcode / Zip" pattern="[0-9]{6}">
-                    <input type="submit" value="GET A QUOTE" class="purchasing__form-btn" >
-                </form>
-                <div class="purchasing__total">
-                    <p class="purchasing__sub">SUB TOTAL <span class="purchasing__sub-price">$ {{total}}</span></p>
-                    <p class="purchasing__grand">GRAND TOTAL <span class="purchasing__grand-price">$ {{total}}</span></p>
-                    <hr>
-                    <button class="purchasing__total-btn">PROCEED TO CHECKOUT</button>
-                </div>
-            </div>
-</section>`,
-    props: ['cart', 'total']
+    </div>`,
+    props: ['cart'],
+    methods: {
+        removeFromCartHandler(id) {
+            this.$emit('remove-from-cart', id)
+        }
+    }
+})
 
+Vue.component('cart-confirm', {
+    template: `<div class="purchasing__confirm">
+        <form class="purchasing__form" action="#" method="POST">            
+            <span class="purchasing__form-heading">SHIPPING ADRESS</span>
+            <input type="text" class="purchasing__form-inp" placeholder="Bangladesh">
+            <input type="text" class="purchasing__form-inp" placeholder="State">
+            <input type="text" class="purchasing__form-inp" placeholder="Postcode / Zip" pattern="[0-9]{6}">
+            <input type="submit" value="GET A QUOTE" class="purchasing__form-btn">
+        </form>
+        <div class="purchasing__total">
+            <p class="purchasing__sub">SUB TOTAL <span class="purchasing__sub-price">$ {{totalPrice}}</span></p>
+            <p class="purchasing__grand">GRAND TOTAL <span class="purchasing__grand-price">$ {{totalPrice}}</span></p>
+            <hr>
+            <button class="purchasing__total-btn">PROCEED TO CHECKOUT</button>
+        </div>
+    </div>`,
+    props: ['total-price']
 })
 
 Vue.component('index-page', {
     template: `<div>
-        <site-header @go-to="goToHandler" v-bind:count="count"></site-header>
+        <site-header @go-to="goToHandler" v-bind:total-quantity="totalQuantity"></site-header>
         <main>
             <promo></promo>
             <categories></categories>
@@ -331,16 +342,18 @@ Vue.component('index-page', {
             this.$emit('add-to-cart', id)
         }
     },
-    props: ['list', 'count']
+    props: ['list', 'total-quantity']
 })
 
 Vue.component('cart-page', {
     template: `<div>
-    <site-header @go-to="goToHandler" v-bind:count="count"></site-header>
+    <site-header @go-to="goToHandler" v-bind:total-quantity="totalQuantity"></site-header>
     <main>
         <cart-top></cart-top>
-        <purchasing v-bind:cart="cart" v-bind:total="total"></purchasing>
-
+        <section class="purchasing center">
+            <cart-products-list v-bind:cart="cart" @remove-from-cart="removeFromCartHandler"></cart-products-list>
+            <cart-confirm v-bind:total-price="totalPrice"></cart-confirm>
+        </section>
     </main>
     <advantages></advantages>
     <feedback></feedback>
@@ -349,9 +362,12 @@ Vue.component('cart-page', {
     methods: {
         goToHandler(target) {
             this.$emit('go-to', target)
+        },
+        removeFromCartHandler(id) {
+            this.$emit('remove-from-cart', id)
         }
     },
-    props: ['cart', 'count', 'total']
+    props: ['cart', 'total-quantity', 'total-price']
 })
 
 const app = new Vue({
@@ -366,7 +382,8 @@ const app = new Vue({
                 "imageS": "item-1-S.jpg",
                 "product_name": "ELLERY X M'O CAPSULE",
                 "product_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-                "price": 52
+                "price": 52,
+                "quantity": 0
             },
             {
                 "id": 2,
@@ -375,7 +392,8 @@ const app = new Vue({
                 "imageS": "item-2-S.jpg",
                 "product_name": "ELLERY X M'O CAPSULE",
                 "product_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-                "price": 52
+                "price": 52,
+                "quantity": 0
             },
             {
                 "id": 3,
@@ -384,7 +402,8 @@ const app = new Vue({
                 "imageS": "item-3-S.jpg",
                 "product_name": "ELLERY X M'O CAPSULE",
                 "product_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-                "price": 52
+                "price": 52,
+                "quantity": 0
             },
             {
                 "id": 4,
@@ -393,7 +412,8 @@ const app = new Vue({
                 "imageS": "item-4-S.jpg",
                 "product_name": "ELLERY X M'O CAPSULE",
                 "product_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-                "price": 52
+                "price": 52,
+                "quantity": 0
             },
             {
                 "id": 5,
@@ -402,7 +422,8 @@ const app = new Vue({
                 "imageS": "item-5-S.jpg",
                 "product_name": "ELLERY X M'O CAPSULE",
                 "product_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-                "price": 52
+                "price": 52,
+                "quantity": 0
             },
             {
                 "id": 6,
@@ -411,7 +432,8 @@ const app = new Vue({
                 "imageS": "item-6-S.jpg",
                 "product_name": "ELLERY X M'O CAPSULE",
                 "product_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-                "price": 52
+                "price": 52,
+                "quantity": 0
             }
         ],
         cart: [],
@@ -422,14 +444,44 @@ const app = new Vue({
             this.currentPage = target
         },
         addToCartHandler(id) {
-            const product = this.list.find(item => item.id === id);
-            this.cart.push(product);
-            console.log(this.cart);
+            // const findProduct = this.list.find(item => item.id === id);
+            // if (findProduct) {
+            //     if (findProduct.quantity > 1) {
+            //         findProduct.quantity++;
+            //     } else {
+            //         this.cart.push(findProduct);
+            //     }
+            // }
+            const findProduct = this.list.find(item => item.id === id);
+            console.log(findProduct);
+            if (findProduct.quantity === 0) {
+                this.cart.push(findProduct);
+                findProduct.quantity++;
+            } else {
+                findProduct.quantity++;
+            }
+
+        },
+        removeFromCartHandler(id) {
+            const findProduct = this.cart.find(item => item.id === id);
+            if (findProduct) {
+                if (findProduct.quantity > 1) {
+                    findProduct.quantity--;
+                } else {
+                    this.cart = this.cart.filter(item => item.id !== id);
+                }
+            }
         }
     },
     computed: {
-        total: function () {
-            return this.cart.reduce((acc, product) => acc + product.price, 0)
+        totalSum: function () {
+            return this.cart.reduce((acc, product) => acc + product.price, 0);
+        },
+        totalQuantity: function () {
+            return this.cart.reduce((acc, product) => acc + product.quantity, 0)
+        },
+        totalPrice: function () {
+            return this.totalSum * this.totalQuantity;
         }
     }
 })
